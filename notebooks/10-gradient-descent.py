d4d48f2f500b30324211f36bfd27fae8c21e4dec
Sumedh Karajagi <122140290+sumedh1k@users.noreply.github.com>
2025-06-28 21:57:45 -0700

    Finish linear regression notebook

--- notebooks/10-gradient-descent.py
+++ notebooks/10-gradient-descent.py
@@ -1,6 +1,6 @@
 import marimo
 
-__generated_with = "0.14.6"
+__generated_with = "0.13.11"
 app = marimo.App(width="medium")
 
 
--- sumedh-notebooks/linear-regression.py
+++ sumedh-notebooks/linear-regression.py
@@ -1,12 +1,48 @@
 import marimo
 
-__generated_with = "0.14.6"
+__generated_with = "0.13.11"
 app = marimo.App(width="full")
+
+
+@app.cell
+def _(mo):
+    mo.md(
+        """
+    ### Linear Regression
+
+
+    1. Define the loss function:
+
+    \[
+    loss(m) = \sum_{i=1}^{n} (y_i - m x_i)^2
+    \]
+
+    2. Find the derivative of the loss function with respect to \( m \):
+
+    \[
+    dloss(m) = \\frac{d}{dm} \sum_{i=1}^{n} (y_i - m x_i)^2 = -2 \sum_{i=1}^{n} (y_i - m x_i) x_i
+    \]
+
+    3. Find the root of the derivative function:
+
+    Use a numerical method like the bisection method or the Newton-Raphson method to solve:
+
+    \[
+    dloss(m) = 0
+    \]
+
+    4. Result:
+
+    The value of \( m \) that satisfies this condition is the most optimal slope of the best-fit line.
+    """
+    )
+    return
 
 
 @app.cell
 def _(plt, x, y):
     plt.scatter(x,y)
+    plt.title("Dataset")
     return
 
 
@@ -27,23 +63,41 @@ def _():
 @app.cell
 def _(dloss, np, plt, sns):
     _x1 = np.arange(0, 11, 0.01)
-    dlossVals = [dloss(t) for t in _x1]
+    dlossVals = [dloss(m) for m in _x1]
     sns.lineplot(x=_x1, y=dlossVals)
-    plt.title(label=f"Derivative of Loss Function: loss(m)")
+    plt.title(label=f"Derivative of Loss Function: dloss(m)")
+    return (dlossVals,)
+
+
+@app.cell
+def _(bisection, dloss, dlossVals, np, plt, sns):
+    _x1 = np.arange(0, 11, 0.01)
+    _x2 = np.random.rand(10)*25 - 10
+    _y2 = np.array([dloss(m) for m in _x2])
+    x0,x1 = np.random.choice(_x2[_y2<0]),np.random.choice(_x2[_y2>0])
+
+    bsearch = np.array(bisection(dloss,x0,x1))
+    sns.lineplot(x=_x1,y=dlossVals)
+
+    alphas=np.linspace(0.3, 1, len(bsearch) )
+    colors = [(1, 0, 0, a) for a in alphas]
+
+    sns.scatterplot(x=bsearch, y=[dloss(m) for m in bsearch], marker="o", color=colors, label="root approximations")
+    sns.scatterplot( x=np.array([x0,x1]), y=np.array([dloss(x0), dloss(x1)]), color='black', label="x0,x1" )
+    plt.title("Bisection Method")
+
     return
 
 
 @app.cell
-def _(bisection, np, x, y):
+def _(np, x, y):
     def loss(m):
         return np.sum((y-m*x)**2)
 
     def dloss(m):
         return -2*np.sum((y-m*x)*x)
 
-    slope = bisection(dloss, 0, 5)[-1]
 
-    print(slope)
     return dloss, loss
 
 
@@ -56,6 +110,8 @@ def _(np):
     Y = [19.7487, 10.0665, 21.0868, 18.7334, 1.9982, 25.1281, 18.0949, 19.4462, 2.2528, 10.9206, 10.1103, 21.4418, 16.5311, 20.8068, 10.4912, 4.2349, 13.9367, 1.0659, 20.9235, 15.8135, 20.554, 8.6238, 23.244, 22.5073, 19.6796, 6.2252, 12.5031, 1.452, 5.3205, 15.8875, 17.9793, 23.2612, 7.7558, 7.8848, 12.374, 4.5146, 1.7772, 10.877, 5.9862, 17.5835, 12.9255, 23.7308, 17.921, 6.8196, 18.6744, 20.3868, 8.874, 6.7928, 16.4503, 3.353, 6.0637, 0.3411, 19.5145, 15.5856, 15.9545, 19.0319, 11.4191, 15.9865, 3.6252, 3.846, 16.2108, 10.5925, 13.1658, 18.3997, 17.9964, 13.0181, 14.8187, 6.6958, 1.702, 11.3029, 5.208, 10.1725, 20.6803, 6.2946, 1.0026, 5.809, 6.0619, 16.7205, 15.5049, 19.7574, 16.4892, 10.4455, 21.6565, 4.3937, 0.1569, 3.3575, 18.4877, 13.0827, 4.215, 11.3017, 2.4396, 19.0589, 12.8776, 9.346, 7.1546, 17.2185, 7.9383, 1.2965, 3.5935, 23.6528]
 
     y = np.array(Y)
+
+
     return x, y
 
 
@@ -69,10 +125,10 @@ def _():
     from numerical.roots import newtonraphson,bisection 
 
     import seaborn as sns
-    sns.set_style("darkgrid")
+    sns.set_theme(style='darkgrid', palette='deep')
 
     import matplotlib.pyplot as plt
-    return bisection, np, plt, sns
+    return bisection, mo, np, plt, sns
 
 
 if __name__ == "__main__":
